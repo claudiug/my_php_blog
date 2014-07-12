fb357577e0c0113df34915f57d489000bd03e0f7
claudiu <claudiu.garba@gmail.com>
2014-07-12 19:23:47 +0200

    slim framework

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-print_r(get_loaded_extensions());
+require 'vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$app->get('/hello/:name', function($name){
+  echo "Hello $name";
+});
+
+$app->run();
 ?>
